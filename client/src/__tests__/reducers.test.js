2696faef4461470a87b296f143a2e84c0076c716
Jose Mejia <mejialaguna@yahoo.com>
2021-12-14 19:34:24 -0500

    global-state

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -1,3 +1,6 @@
+import { reducer } from "../utils/reducers";
+
+
 // import our actions
 import {
   UPDATE_PRODUCTS,
@@ -11,3 +14,39 @@ const initialState = {
   categories: [{ name: "Food" }],
   currentCategory: "1",
 };
+
+test("UPDATE_PRODUCTS", () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_PRODUCTS,
+    //type: This is the type of action we're performing, and should be one of the predefined actions we created earlier.
+    products: [{}, {}],
+    //   value(in this case products): This won't always have the name value, but it is a name representative of the new data we want to use   with the action.
+  });
+
+  expect(newState.products.length).toBe(2);
+  expect(initialState.products.length).toBe(0);
+});
+
+test("UPDATE_CATEGORIES", () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CATEGORIES,
+    //type: This is the type of action we're performing, and should be one of the predefined actions we created earlier.
+    categories: [{}, {}],
+    //   value(in this case categories): This won't always have the name value, but it is a name representative of the new data we want to use   with the action.
+  });
+  // The result of the reducer() should show that the length of our updated categories array will be 2, while the initial categories array should still be 1. This indicates that we didn't affect our original state values at all and simply used it to create a new version of it.
+
+  expect(newState.categories.length).toBe(2);
+  expect(initialState.categories.length).toBe(1);
+});
+
+
+test("UPDATE_CURRENT_CATEGORY", () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CURRENT_CATEGORY,
+    currentCategory: "2",
+  });
+
+  expect(newState.currentCategory).toBe("2");
+  expect(initialState.currentCategory).toBe("1");
+});
---
+++ client/src/utils/reducers.js
@@ -0,0 +1,34 @@
+import {
+  UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+} from "./actions";
+
+
+// Adding this code to the reducers.js file imports the possible actions we can perform and creates a function called reducer(). When the function executes, we pass the value of the action.type argument into a switch statement and compare it to our possible actions.
+export const reducer = (state, action) => {
+  switch (action.type) {
+    // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
+    case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products],
+      };
+    // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+    // if action type value is the value of `UPDATE_CURRENT_CATEGORY`, return a new state object with an updated categories array
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
+
+    // if it's none of these actions, do not update state at all and keep things the same!
+    default:
+      return state;
+  }
+};
