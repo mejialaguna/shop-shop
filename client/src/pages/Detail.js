aee19755ff8976d0713deca339a6c997c7143b92
Jose Mejia <mejialaguna@yahoo.com>
2021-12-21 10:47:47 -0500

    cartActions

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from "../utils/actions";
 
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
@@ -41,7 +46,7 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
+            <button >Add to Cart</button>
             <button>Remove from Cart</button>
           </p>
 
